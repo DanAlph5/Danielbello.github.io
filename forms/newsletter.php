87d94b6954b72d342360953fe2f3820ad890e996
DanAlph5 <85260857+DanAlph5@users.noreply.github.com>
2025-03-03 17:20:33 +0100

    Update newsletter.php

--- forms/newsletter.php
+++ forms/newsletter.php
@@ -23,12 +23,12 @@
   $contact->from_email = $_POST['email'];
   $contact->subject ="New Subscription: " . $_POST['email'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  // // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // /*
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'smtp.gmail.com',
+    'username' => 'ddanieldamilola05@gmail.com',
+    'password' => 'xbjswxcpkvcjgvwx',
     'port' => '587'
   );
   */

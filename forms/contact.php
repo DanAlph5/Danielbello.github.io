c917be1c0760e6140627909c4e9cd294f2074227
DanAlph5 <85260857+DanAlph5@users.noreply.github.com>
2025-03-03 17:19:54 +0100

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -28,7 +28,7 @@
   $contact->smtp = array(
     'host' => 'smtp.gmail.com',
     'username' => 'ddanieldamilola05@gmail.com',
-    'password' => 'xbjs wxcp kvcj gvwx',
+    'password' => 'xbjswxcpkvcjgvwx',
     'port' => '587'
   );
   */
